344a11f5a5dbdad93b81fa307856aadbb503691c
CodeWizrd <codewizard001@gmail.com>
2022-06-02 11:34:34 +0530

    Added Enumerating options

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import tqdm
 import sys
 import os
 
@@ -8,7 +9,36 @@ import re
 
 import requests
 
-url = "https://dl3.3rver.org/cdn2/03/series/2017/Money.Heist/S03"
+def get_res(links) :
+    options = ['ALL']
+    all = ['480', '720', '1080', '2160']
+    keys = {
+        'ALL' : 0,
+        '480' : 1,
+        '720' : 2,
+        '1080' : 3,
+        '2160' : 4
+    }
+    for link in links :
+        for option in all :
+            if option in link :
+                options.append(option)
+    options = list(set(options))
+    options.sort(key=keys.get)
+    return options
+
+def get_links(links,option) :
+    res = []
+    if option == 'ALL' :
+        return list(links)
+    for link in links :
+        if option in link :
+            res.append(link)
+    return res
+
+OUTPUT_FOLDER = "output/"
+
+url = "https://dl3.3rver.org/cdn2/03/series/2017/Money.Heist/S03/"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36"
     }
@@ -27,6 +57,25 @@ for link in soup.findAll('a'):
     links.append(link.get('href'))
     l[link.get('href')] = link.text
 
-for link in l:
-    if link.endswith('.mp4'):
+options = get_res(links)
+
+print(f'[+] Options:')
+for option in options:
+    print(f'[{options.index(option):>2d}] {option}')
+
+try :
+    option = options[int(input('[+] Choose an option: '))]
+except :
+    print('[-] Invalid option')
+    option = 'ALL'
+
+required_links = get_links(links,option)
+
+for link in required_links:
+    if link.endswith('.mp4') or link.endswith('.mkv'):
         print(f'[+] {l[link]}')
+
+if not os.path.exists(OUTPUT_FOLDER):
+    os.makedirs(OUTPUT_FOLDER)
+
+f = required_links[0]
